645e63fd57870731895330d6d64e76f75eb22b09
Oncioiu Costin <oncioiu.costi@gmail.com>
2017-01-04 15:36:37 +0200

    some changes

--- app/src/main/java/android/costi/bucketdrops/ActivityMain.java
+++ app/src/main/java/android/costi/bucketdrops/ActivityMain.java
@@ -3,13 +3,17 @@ package android.costi.bucketdrops;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
 public class ActivityMain extends AppCompatActivity {
 
     Toolbar mainToolbar;
+    Button addButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,13 +22,19 @@ public class ActivityMain extends AppCompatActivity {
         mainToolbar=(Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(mainToolbar);
         initBackgroundImage();
+        addButton= (Button) findViewById(R.id.btn_add_drop);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(ActivityMain.this, "Buttonul a fost apasat", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     private void initBackgroundImage() {
         ImageView background=(ImageView) findViewById(R.id.iv_background);
         Glide.with(this)
                 .load(R.drawable.background)
-                .centerCrop()
                 .into(background);
     }
 }
